f25db0ab8f2f5ca81f26ca2a2f0149688a64a18e
DESKTOP-UGOJQLJ\IT&E Department <mayukhpuspenchatterjee12@gmail.com>
2023-06-10 09:30:23 +0530

    Revert "Practice C programmes"
    This reverts commit 77b937506a3527cfa0248a6f23e5fb7d25e0e860.

--- sampleInput.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-    int a;
-    printf("Please enter a number:");
-    scanf("%d",&a);
-    printf("a=%d",a);
-} 
--- userinput.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int a;
-    printf("please enter a value for a:");
-    scanf("%d",&a);
-    printf("a=%d",a);
-}
-
